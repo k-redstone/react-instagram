82978e99ddd0e7833b7aae4440d7d81378a0d9a2
k-redstone <che06189@gmail.com>
2024-04-16 15:13:03 +0900

    Refactor: 중첩 router 사용을 위해 layout 구조 변경

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,15 @@
+import MainLayout from "../../layout/MainLayout"
+
+const ProfilePage = () => {
+
+  return(
+    <div>
+      <p>asdfs</p>
+
+    </div>
+      // <MainLayout>
+      // </MainLayout>
+  )
+}
+
+export default ProfilePage

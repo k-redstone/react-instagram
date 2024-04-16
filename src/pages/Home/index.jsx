8bf4ac3e28eeb9e753375ac19d26647ad582bf7d
k-redstone <che06189@gmail.com>
2024-04-16 10:47:02 +0900

    Feat: 팔로잉 추천 레이아웃 작업

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import GlobalNav from "../../layout/GlobalNav";
 import Article from "../../components/Article";
 import jsonData from '../../assets/data.json';
+import RecommendUser from "../../layout/RecommendUser";
 
 
 const HomePage = () => {
@@ -10,9 +11,14 @@ const HomePage = () => {
   return (
     <div className="flex">
       <GlobalNav />
-      <main className="ml-[335px]">
-        <Article data={jsonData} />
-      </main>
+      <div className="flex w-full justify-center">
+        <main className="ml-[335px]">
+          <Article data={jsonData} />
+        </main>
+        <div className="pl-20">
+          <RecommendUser />
+        </div>
+      </div>
     </div>
   );
 };

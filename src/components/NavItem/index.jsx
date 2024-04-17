f5da47d2a9e42778c5316ddd2f2c9a2778a2ef5a
k-redstone <che06189@gmail.com>
2024-04-17 11:22:38 +0900

    Feat: Navitem 각 요소 라우터 연결

--- src/components/NavItem/index.jsx
+++ src/components/NavItem/index.jsx
@@ -1,13 +1,16 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const NavItem = ({ icon, text }) => {
+const NavItem = ({ icon, text, url }) => {
   return (
-    <li className="group flex items-center justify-start py-4 rounded-md pl-2 hover:bg-gray-200 hover:cursor-pointer">
-      <div className="box-border text-2xl mr-5 group-hover:scale-110 group-hover:transition-all">
-        {icon}
-      </div>
-      <span className="peer text-base">{text}</span>
-    </li>
+    <Link to={url}>
+      <li className="group flex items-center justify-start py-4 rounded-md pl-2 hover:bg-gray-200 hover:cursor-pointer">
+        <div className="box-border text-2xl mr-5 group-hover:scale-110 group-hover:transition-all">
+          {icon}
+        </div>
+        <span className="peer text-base">{text}</span>
+      </li>
+    </Link>
   );
 };
 
--- src/layout/GlobalNav/index.jsx
+++ src/layout/GlobalNav/index.jsx
@@ -9,26 +9,29 @@ import {
 } from "react-icons/ai";
 import { MdOutlineExplore, MdOndemandVideo } from "react-icons/md";
 import NavItem from "../../components/NavItem";
+import { Link } from "react-router-dom";
 
 
 const GlobalNav = () => {
   return(
     <nav className="hidden md:flex md:w-[72px] lg:w-[220px] xl:w-[335px] fixed min-h-screen  flex-col px-4 border-r-2 ">
-      <img
-        className="w-[103px] h-[29px] mt-10 ml-2"
-        src="/images/logo_instagram.png"
-        alt="logo"
-      />
+      <Link to={'/'}>
+        <img
+          className="w-[103px] h-[29px] mt-10 ml-2"
+          src="/images/logo_instagram.png"
+          alt="logo"
+        />
+      </Link>
       <div className="h-nav-list-height flex flex-col justify-between">
         <ul className="mt-10">
-          <NavItem icon={<AiOutlineHome />} text={"홈"} />
-          <NavItem icon={<AiOutlineSearch />} text={"검색"} />
-          <NavItem icon={<MdOutlineExplore />} text={"탐색 탭"} />
-          <NavItem icon={<MdOndemandVideo />} text={"릴스"} />
-          <NavItem icon={<AiOutlineMessage />} text={"메시지"} />
-          <NavItem icon={<AiOutlineHeart />} text={"알림"} />
-          <NavItem icon={<AiOutlinePlusSquare />} text={"만들기"} />
-          <NavItem icon={<AiOutlineUser />} text={"프로필"} />
+          <NavItem icon={<AiOutlineHome />} text={"홈"} url={'/'}/>
+          <NavItem icon={<AiOutlineSearch />} text={"검색"} url={'/'} />
+          <NavItem icon={<MdOutlineExplore />} text={"탐색 탭"} url={'/'} />
+          <NavItem icon={<MdOndemandVideo />} text={"릴스"} url={'/'}  />
+          <NavItem icon={<AiOutlineMessage />} text={"메시지"} url={'/'}  />
+          <NavItem icon={<AiOutlineHeart />} text={"알림"} url={'/'}  />
+          <NavItem icon={<AiOutlinePlusSquare />} text={"만들기"} url={'/'}  />
+          <NavItem icon={<AiOutlineUser />} text={"프로필"} url={'/profile'}  />
         </ul>
         <div className="mt-10">
           <NavItem icon={<AiOutlineMore />} text={"더보기"} />
